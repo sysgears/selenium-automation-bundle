dc43a3c7c4f4eba2eb4e1af9e27cb70dffdc25da
Nikita Riabchenko <unrealgear@gmail.com>
2018-04-20 16:13:15 +0300

    Modified BaseTest to start a browser with a proxy before functional tests.

--- src/test/groovy/com/sysgears/seleniumbundle/common/BaseTest.groovy
+++ src/test/groovy/com/sysgears/seleniumbundle/common/BaseTest.groovy
@@ -5,7 +5,9 @@ import com.codeborne.selenide.Configuration
 import com.codeborne.selenide.WebDriverRunner
 import com.sysgears.seleniumbundle.core.conf.Config
 import com.sysgears.seleniumbundle.core.data.DataMapper
+import com.sysgears.seleniumbundle.core.proxy.BrowserProxy
 import com.sysgears.seleniumbundle.core.webdriver.DriverInitializer
+import net.lightbody.bmp.BrowserMobProxyServer
 import org.testng.annotations.*
 
 import static com.codeborne.selenide.Selenide.*
@@ -27,6 +29,9 @@ class BaseTest {
      * Test data mapper, is responsible for test data preparation for TestNG data providers.
      */
     protected DataMapper mapper = new DataMapper()
+
+
+    protected BrowserProxy browserProxy
 
     /**
      *  OS that is used for test execution.
@@ -62,9 +67,17 @@ class BaseTest {
      */
     @BeforeClass(alwaysRun = true, dependsOnMethods = "setupGlobalParameters")
     void initSelenideWebDriverRunner() {
-        def driver = !conf.gridUrl ?
-                DriverInitializer.createDriver(browser) :
-                DriverInitializer.createRemoteDriver(conf.gridUrl, os, browser)
+        def driver
+
+        if (this.class.getSuperclass().getAnnotation(Test).groups().contains("functional")) {
+            browserProxy = new BrowserProxy(new BrowserMobProxyServer())
+            driver = DriverInitializer.createChromeWithProxy(browserProxy.seleniumProxy)
+        } else if (conf.gridUrl) {
+            driver = DriverInitializer.createRemoteDriver(conf.gridUrl, os, browser)
+        } else {
+            driver = DriverInitializer.createDriver(browser)
+        }
+
         driver.manage().window().maximize()
         WebDriverRunner.setWebDriver(driver)
     }
