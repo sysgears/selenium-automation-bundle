7c4fb8e617442474deb6dcd2a18b50f2d2b41d5c
Yuriy Vasylenko <yuriy.vasylenko@sysgears.com>
2018-08-10 11:35:58 +0300

    Minor corrections: removed withBrowserMobProxy flag.

--- src/main/resources/config/ApplicationProperties.groovy
+++ src/main/resources/config/ApplicationProperties.groovy
@@ -5,7 +5,6 @@ package config
 baseUrl = "https://google.co.uk"  // the URL to website under the test
 os = System.getProperty("os.name").split(/\s/).first().toLowerCase() // OS identifier to configure environment
 remoteUrl = System.getProperty("test.remoteUrl") ?: null                     // a link to Selenium Grid
-withBrowsermobProxy = true          // adds Browsermob Proxy to drivers, can be set to false
 
 browser {
     name = "chrome" // default browser to execute tests (chrome, firefox, microsoftedge, headless)
