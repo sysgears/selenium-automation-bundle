4c07bf44fedca140b203be3b54d87b00c0fa5feb
Yuriy Vasylenko <yuriy.vasylenko@sysgears.com>
2018-10-05 12:48:09 +0300

    Added UI tests example for layout with static changes, changed package structure for web page resources, changed gradle ignoreFailures option to true.

--- src/main/groovy/com/sysgears/seleniumbundle/pagemodel/GooglePage.groovy
+++ src/main/groovy/com/sysgears/seleniumbundle/pagemodel/GooglePage.groovy
@@ -1,10 +1,12 @@
 package com.sysgears.seleniumbundle.pagemodel
 
+import com.codeborne.selenide.Selenide
 import com.codeborne.selenide.SelenideElement
 import com.sysgears.seleniumbundle.core.pagemodel.AbstractPage
 import com.sysgears.seleniumbundle.core.pagemodel.annotations.StaticElement
 import com.sysgears.seleniumbundle.core.uicomparison.UIComparison
 import io.qameta.allure.Step
+import org.apache.commons.io.FilenameUtils
 import org.openqa.selenium.By
 
 import static com.codeborne.selenide.Selenide.$
@@ -12,7 +14,8 @@ import static com.codeborne.selenide.Selenide.$
 class GooglePage extends AbstractPage<GooglePage> implements UIComparison<GooglePage> {
 
     @StaticElement
-    private SelenideElement queryField = $(By.name("q"))
+    private SelenideElement queryField = $(By.name("q")),
+                            logoImg = $(By.id("hplogo"))
 
     GooglePage() {
         this.url = "/"
@@ -43,5 +46,19 @@ class GooglePage extends AbstractPage<GooglePage> implements UIComparison<Google
     @Step("Submit search")
     private void submit() {
         queryField.pressEnter()
+    }
+
+    @Step("Replace logo image")
+    GooglePage replaceLogo() {
+        def path = FilenameUtils.separatorsToSystem("src/test/resources/data/google/colorfulLogo.txt")
+
+        Selenide.executeJavaScript("arguments[0].src='${new File(path).text}'; arguments[0].srcset=''", logoImg)
+        this
+    }
+
+    @Step("Remove focus from query field")
+    GooglePage removeFocusFromQueryField() {
+        queryField.pressEscape()
+        this
     }
 }
--- src/test/groovy/com/sysgears/seleniumbundle/tests/ui/UITestExample.groovy
+++ src/test/groovy/com/sysgears/seleniumbundle/tests/ui/UITestExample.groovy
@@ -1,15 +1,44 @@
 package com.sysgears.seleniumbundle.tests.ui
 
 import com.sysgears.seleniumbundle.common.UITest
-import com.sysgears.seleniumbundle.pagemodel.ShopidaiPage
+import com.sysgears.seleniumbundle.pagemodel.GooglePage
 import org.testng.annotations.BeforeMethod
+import org.testng.annotations.Test
 
 class UITestExample extends UITest {
 
-    protected ShopidaiPage shopidaiPage
+    protected GooglePage googlePage
 
     @BeforeMethod
     void openApplication() {
-        shopidaiPage = new ShopidaiPage().setEnvironment(this).open().waitForPageToLoadElements()
+        googlePage = new GooglePage()
+                .setEnvironment(this)
+                .open()
+                .waitForPageToLoadElements()
+
+        // is used to remove instability related to cursor blinking and shadow changes under the query field.
+                .setIgnoredElements(["#tsf"])
+    }
+
+    /**
+     * Checks the layout of the Google search page that wasn't modified.
+     */
+    @Test
+    void checkWithoutChangesInLayout() {
+        googlePage.compareLayout("successful_case")
+    }
+
+    /**
+     * Checks the layout of the Google search page that was modified.
+     */
+    @Test
+    void checkWithChangesInLayout() {
+
+        // the block below changes the default Google logo to custom one if we don't run the tests in the baseline mode
+        if (!conf.baselineMode) {
+            googlePage.replaceLogo()
+        }
+
+        googlePage.compareLayout("case_with_changes")
     }
 }
